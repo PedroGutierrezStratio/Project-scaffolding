e0b6c9c67f37e7570035643e555abea6252ab47d
Pedro Gutiérrez <pgutierrez@stratio.com>
2016-05-03 18:15:11 +0200

    Configured Karma for get the templates.

--- karma.conf.js
+++ karma.conf.js
@@ -13,28 +13,35 @@ module.exports = function(config) {
          ...assets.js,
          ...assets.jsTest,
          path.origin.folder + patterns.allModuleJS,
-         path.origin.folder + patterns.allJS
+         path.origin.folder + patterns.allJS,
+         path.origin.folder + patterns.allHTML
       ],
       preprocessors: {},
       babelPreprocessor: {
          options: {
-           presets: ['es2015'],
-           sourceMap: 'inline'
+            presets: ['es2015'],
+            sourceMap: 'inline'
          }
       },
       reporters: ['progress', 'coverage'],
       coverageReporter: {
          dir: path.coverage.folder,
          reporters: [
-            { type: 'html', subdir: 'html' },
-            { type: 'lcovonly', subdir: '.', file: 'icov.info' }
+            {type: 'html', subdir: 'html'},
+            {type: 'lcovonly', subdir: '.', file: 'icov.info'}
          ]
       },
+      colors: true,
+      ngHtml2JsPreprocessor: {
+         stripPrefix: path.origin.folder,
+         moduleName: path.moduleCoreName
+      }
    };
 
    karmaConf.preprocessors[path.origin.folder + patterns.allModuleJS] = ['babel'];
    karmaConf.preprocessors[path.origin.folder + patterns.allJSExcludeTest] = ['coverage','babel'];
    karmaConf.preprocessors[path.origin.folder + patterns.allTestJS] = ['babel'];
+   karmaConf.preprocessors[path.origin.folder + patterns.allHTML] = ['ng-html2js'];
 
    config.set(karmaConf);
 };
