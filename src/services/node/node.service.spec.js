38808c2367ae6f0adee994cb106d6be5263b4f82
Pedro Gutiérrez <pgutierrez@stratio.com>
2016-05-10 12:37:55 +0200

    Test for Node Service.

--- src/services/node/node.service.spec.js
+++ src/services/node/node.service.spec.js
@@ -1,1 +1,19 @@
-// Spec - Factory - Nodes
+describe('service: NodeService', () => {
+
+   beforeEach(module('app.service'));
+
+   beforeEach(inject(function(_NodeService_) {
+      this.service = _NodeService_;
+   }));
+
+   it('should has a getNodes method witch return a promise', function() {
+      expect(this.service.getNodes).toBeDefined();
+      expect(this.service.getNodes().$$state).toBeDefined();
+   });
+
+   it('should has a getNodeById method witch return a promise', function() {
+      expect(this.service.getNodeById).toBeDefined();
+      expect(this.service.getNodeById().$$state).toBeDefined();
+   });
+
+});
